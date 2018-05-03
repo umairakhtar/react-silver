fa56ad7abb35f58718a71d52f40f31fe92d09e93
Umair <akhtar.u@husky.neu.edu>
2018-05-02 20:40:43 -0400

    Display Projects

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+
+      {
+        title:'Business web',
+        category:'Web Design'
+      },
+      {
+        title:'Social App',
+        category:'Mobile Development'
+      },
+      {
+        title:'Ecommerce Shopping Cart',
+        category:'Web Development'
+      }
+    
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         My App
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
